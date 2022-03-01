897ccc026200ca151d083b12da4a46ee82c6191c
Aapo Raukovaara <s1aara00@students.osao.fi>
2022-03-01 12:28:59 +0200

    juuh

---
+++ script.js
@@ -0,0 +1,5 @@
+function prepositiotYms(sana) {
+    preArtKon = ['amid', 'as', 'at', 'but', 'by', 'down', 'for', 'from', 'in', 'into', 'like', 'near', 'next', 'of', 'off', 'on', 'onto', 'out', 'over', 'past', 'per', 'plus', 'save', 'than', 'till', 'to', 'up', 'upon', 'via', 'with', 'but', 'so', 'or', 'and', 'if', 'till', 'when', 'while', 'a', 'an', 'the']
+
+    return !preArtKon.includes(sana)
+}
